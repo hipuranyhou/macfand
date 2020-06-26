bef989ac741b97c2023dc16c9bea1503700558e7
hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-06-26 14:14:03 +0200

    Fixed renamed t_settings members

--- src/settings.c
+++ src/settings.c
@@ -13,10 +13,10 @@
 #include "monitor.h"
 
 void settings_load_defaults(t_settings *settings) {
-    settings->low_temp = 63;
-    settings->high_temp = 66;
-    settings->max_temp = 84;
-    settings->poll_time = 1;
+    settings->temp_low = 63;
+    settings->temp_high = 66;
+    settings->temp_max = 84;
+    settings->time_poll = 1;
     settings->daemon = 0;
     settings->verbose = 0;
 }
@@ -25,7 +25,7 @@ void settings_set_max_temp(t_settings *settings, const t_node *monitors) {
     t_monitor *monitor = NULL;
     while (monitors) {
         monitor = monitors->data;
-        settings->max_temp = (settings->max_temp > monitor->temp_max) ? settings->max_temp : monitor->temp_max;
+        settings->temp_max = (settings->temp_max > monitor->temp_max) ? settings->temp_max : monitor->temp_max;
         monitors = monitors->next;
     }
 }
