1496761e666e05ebab2835f69aa4858754d1a83c
Hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-06-26 18:50:00 +0200

    Adjusted documentation

--- src/helper.h
+++ src/helper.h
@@ -11,29 +11,30 @@
 #define MACFAND_HELPER_H_pifiohaods
 
 /**
- * @brief 
- * 
- * @param format 
- * @param ... 
- * @return char* 
+ * @brief Creates string using given format and parameters.
+ * Concatenates given paramteres using format with vsnprintf(). Allocates memory for given string which has to
+ * be later freed.
+ * @param[in]  format  Format used for concatenation.
+ * @param[in]  ...     Parameters to concatenate. 
+ * @return char* NULL on error, pointer to null terminated char* otherwise.
  */
 char* concatenate_format(const char* format, ...);
 
 /**
- * @brief 
- * 
- * @param a 
- * @param b 
- * @return int 
+ * @brief Returns max of two given integers.
+ * Returns max of two given integers.
+ * @param[in]  a  Integer a. 
+ * @param[in]  b  Integer b.
+ * @return int The higher value of these two, b if they are the same.
  */
 int max(const int a, const int b);
 
 /**
- * @brief 
- * 
- * @param a 
- * @param b 
- * @return int 
+ * @brief Returns min of two given integers.
+ * Returns min of two given integers.
+ * @param[in]  a  Integer a. 
+ * @param[in]  b  Integer b.
+ * @return int The lower value of these two, b if they are the same.
  */
 int min(const int a, const int b);
 
