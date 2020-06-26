1d27e0644523ecbc0948102681a450c9043b098b
hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-06-26 14:22:19 +0200

    Adjusted documentation

--- src/settings.h
+++ src/settings.h
@@ -13,8 +13,9 @@
 #include "linked.h"
 
 /**
- * @brief 
- * 
+ * @brief Holds macfand settings values.
+ * Used to hold macfand settings, all temperatures used for calculating fan adjust (low, high, max), 
+ * main control loop poll time and if macfand should run in daemon and verbose mode.
  */
 typedef struct settings {
     int temp_low;
@@ -26,15 +27,17 @@ typedef struct settings {
 } t_settings;
 
 /**
- * @brief 
- * 
- * @param settings 
+ * @brief Sets default macfand settings.
+ * Sets default macfand settings.
+ * @param[out]  settings  Pointer to struct holding macfand settings.
  */
 void settings_load_defaults(t_settings *settings);
 
 /**
- * @brief 
- * 
+ * @brief Sets settings->temp_max.
+ * Sets settings->temp_max based on highest value got from temperature monitors (monitor->temp_max).
+ * @param[out]  settings  Pointer to struct holding macfand settings.
+ * @param[in]   monitors  Pointer to head of linked list of temperature monitors.
  */
 void settings_set_max_temp(t_settings *settings, const t_node *monitors);
 
