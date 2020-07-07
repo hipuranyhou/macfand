4ec8d3def37404e041c83569a586604443d8bab7
Hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2020-07-07 22:08:26 +0200

    Started work on config file

---
+++ src/config.c
@@ -0,0 +1,12 @@
+/**
+ * macfand - hipuranyhou - 21.06.2020
+ * 
+ * Daemon for controlling fans on Linux systems using
+ * applesmc and coretemp.
+ * 
+ * https://github.com/Hipuranyhou/macfand
+ */
+
+#include "config.h"
+
+
---
+++ src/config.h
@@ -0,0 +1,15 @@
+/**
+ * macfand - hipuranyhou - 21.06.2020
+ * 
+ * Daemon for controlling fans on Linux systems using
+ * applesmc and coretemp.
+ * 
+ * https://github.com/Hipuranyhou/macfand
+ */
+
+#ifndef MACFAND_CONFIG_H_nuviuvnsiv
+#define MACFAND_CONFIG_H_nuviuvnsiv
+
+
+
+#endif //MACFAND_CONTROL_H_fsdfdsfsdf
