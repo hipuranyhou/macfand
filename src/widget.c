74f79831f99a3378814c3b3e6307ce45a391dc70
Hipuranyhou <49517809+Hipuranyhou@users.noreply.github.com>
2021-01-25 14:10:07 +0100

    Renamed write function and variables

--- src/widget.c
+++ src/widget.c
@@ -16,23 +16,23 @@
 
 
 void wgt_write(const t_node *fans) {
-    FILE *widget_file = NULL;
-    t_fan *fan = NULL;
+    FILE  *file = NULL;
+    t_fan *fan  = NULL;
 
-    widget_file = fopen(set_get_val_str(SET_WIDGET_FILE_PATH), "w");
-    if (!widget_file) {
+    file = fopen(set_get_val_str(SET_WIDGET_FILE_PATH), "w");
+    if (!file) {
         logger_log(LOG_L_ERROR, "%s", "Unable to open widget file");
         return;
     }
 
     while (fans) {
         fan = fans->data;
-        if (fprintf(widget_file, "%d(f%d)%c", fan->spd.real, fan->id, (fans->next) ? ' ' : '\0') < 0)
+        if (fprintf(file, "%d(f%d)%c", fan->spd.real, fan->id, (fans->next) ? ' ' : '\0') < 0)
             logger_log(LOG_L_ERROR, "%s %d %s", "Unable to write speed of fan", fan->id, "to widget file");
         fans = fans->next;
     }
 
-    if (fclose(widget_file) == EOF) {
+    if (fclose(file) == EOF) {
         logger_log(LOG_L_ERROR, "%s", "Unable to close widget file");
         return;
     }
